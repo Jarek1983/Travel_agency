5fc027c59275840dd9e545bb583e72904ea4b737
Jaroslaw Zmijewski <jzmija@wp.pl>
2019-10-10 09:12:59 +0200

    Add validates for excursion's model

--- app/models/excursion.rb
+++ app/models/excursion.rb
@@ -1,2 +1,5 @@
 class Excursion < ApplicationRecord
+  validates :country, :city, presence: true, length: {minimum: 2}
+  validates :description, :particulars, presence: true, length: {minimum: 10}
+  validates :price, presence: true
 end
